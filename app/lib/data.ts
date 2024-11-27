21e518d899878eb26da624599d4ff776cd186ca9
unknown <kufany@gmailcom>
2024-11-28 03:28:20 +0900

    업데이트

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -34,12 +34,12 @@ export async function fetchLatestInvoices() {
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
       FROM invoices
       JOIN customers ON invoices.customer_id = customers.id
-      ORDER BY invoices.date DESC
+      ORDER BY invoices.amount  DESC
       LIMIT 5`;
 
     const latestInvoices = data.rows.map((invoice) => ({
       ...invoice,
-      amount: formatCurrency(invoice.amount),
+      amount: formatCurrency(invoice.amount), 
     }));
     return latestInvoices;
   } catch (error) {
